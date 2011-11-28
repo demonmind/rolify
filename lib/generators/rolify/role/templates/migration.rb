5c093f4da25b0a79eca6b2e30ddb14fe59cd2444
Kleon Kita <kleon@vitrue.com>
2011-11-28 00:06:10 -0500

    Added drop for rails3.0.x

--- lib/generators/rolify/role/templates/migration.rb
+++ lib/generators/rolify/role/templates/migration.rb
@@ -16,4 +16,12 @@ class RolifyCreate<%= role_cname.pluralize.camelize %> < ActiveRecord::Migration
     add_index(:<%= role_cname.tableize %>, [ :name, :resource_type, :resource_id ])
     add_index(:<%= "#{user_cname.tableize}_#{role_cname.tableize}" %>, [ :<%= user_cname.underscore.singularize %>_id, :<%= role_cname.underscore.singularize %>_id ])
   end
+  
+  def self.down
+    remove_index(:<%= role_cname.tableize %>, :name)
+    remove_index(:<%= role_cname.tableize %>, [ :name, :resource_type, :resource_id ])
+    remove_index(:<%= "#{user_cname.tableize}_#{role_cname.tableize}" %>, [ :<%= user_cname.underscore.singularize %>_id, :<%= role_cname.underscore.singularize %>_id ])
+    remove_table(:<%= (user_cname.tableize + "_" + role_cname.tableize) %>)
+    remove_table(:<%= role_cname.tableize %>)
+  end
 end
